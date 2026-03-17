7186df4b26cf1d89ffb3061fd9b9e8d65c35412a
Neftaly123 <fuentesneftali123@gmail.com>
2026-03-17 13:46:23 -0600

    cambios

--- lab1-C2/vite.config.js
+++ lab1-C2/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'laboratorio-1-Blo-2',
   plugins: [
     vue(),
     vueDevTools(),
